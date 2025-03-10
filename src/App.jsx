5b8df442496db0b9e41fe981dfdff751926ff6d7
vanDerBC <tobiasbc@hiof.no>
2025-03-10 14:49:25 +0100

    oppdaterer til html
    la til en funksjon i App.jsx som gjør at når nettsiden oppdateres, vil den vise html, selv om den viser en annen side

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './components/Layout'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import Resources from './components/Resources'
 import PageTitle from './components/PageTitle'
 
 function App() {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Hvis pathen ikke er "/" (altså hvis den er en kategori-side), naviger til "/"
+    if (location.pathname !== "/") {
+      navigate("/", { replace: true });
+    }
+  }, []); // Tom array sikrer at dette kun skjer ved første render (f.eks. etter refresh)
 
   return (
     <Layout>
